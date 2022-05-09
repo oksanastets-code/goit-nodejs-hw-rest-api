9be4a4483da548fc2af7995f8eb74367ba4f8f27
Oksana Stets <oksanastets73@gmail.com>
2022-05-09 17:32:41 +0300

    add update subscription

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,9 +2,9 @@ const { Contact } = require("../../models")
 
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite} = req.query;
+  const { page = 1, limit = 20, favorite = true} = req.query;
   const skip = (page - 1) * limit;
-  
+
   const contacts = await Contact.find({ owner: _id, favorite: favorite }, "", { skip, limit: Number(limit) })
     .populate("owner", "_id email")
     .exec();
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,13 @@ const { users: ctrl } = require("../../controllers")
 const router = express.Router();
 
 router.post("/signup", validation(joiSchema), ctrlWrapper(ctrl.signup));
-router.post("/login", validation(joiSchema), ctrlWrapper(ctrl.login))
-router.get("/current", auth, ctrlWrapper(ctrl.getCurrent))
-router.get("/logout", auth, ctrlWrapper(ctrl.logout))
+
+router.post("/login", validation(joiSchema), ctrlWrapper(ctrl.login));
+
+router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
+
+router.get("/logout", auth, ctrlWrapper(ctrl.logout));;
+
+router.patch("/", auth, ctrlWrapper(ctrl.updateSubscription));
+
 module.exports = router;
